2295b1953ba3d8e25ac3847fb1c97ae210ae69db
jjoylee <ekdnlt1995@gmail.com>
2018-07-23 17:16:08 +0900

    Palindromes

---
+++ Palindromes/Main.java
@@ -0,0 +1,31 @@
+package algorithm.Palindromes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static String vocabulary;
+	public static void main(String[] args) throws IOException{
+		getVocabulary();
+		System.out.println(isPalindrome());
+	}
+	
+	private static boolean isPalindrome() {
+		if(vocabulary.length() == 1) return true;
+		int start = 0;
+		int end = vocabulary.length() - 1;
+		while(start < end){
+			if(vocabulary.charAt(start++) != vocabulary.charAt(end--)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static void getVocabulary() throws IOException {
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		vocabulary = br.readLine();
+		br.close();
+	}
+}
